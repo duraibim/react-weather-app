d37fe340d1dd81585cc0b597b6b200979bb08793
Manal <duraibimn@gmail.com>
2021-06-28 16:10:57 -0500

    created react weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <div className="container">
+      <Weather defaultCity="New York" />
+
+        <footer>
+        This project was coded by Manal Alduraibi🐞 and is Open-sourced on {""} 
+          <a
+            href=".." 
+            target="_blank"
+            rel="noopener noreferrer"
+          >  GitHub</a> and hosted on <a
+          href=".." 
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        > Netlify</a> 
+          
+          
+          </footer> 
+        </div>
     </div>
   );
 }
